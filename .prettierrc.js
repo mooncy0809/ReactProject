c389a4b6204f78f0cd796bfa20ca6e9a3bbb3adf
mooncy0809 <mooncy0809@naver.com>
2023-01-27 15:55:41 +0900

    mcy

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  singleQuote: true,
+  semi: false,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: 'all',
+  printWidth: 80,
+  jsxSingleQuote: true,
+  arrowParens: 'always',
+  bracketSpacing: true,
+}
